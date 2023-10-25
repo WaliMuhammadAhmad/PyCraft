ae6c10dc11fabe17d2a122390d92e4e394193333
Wali Muhammad Ahmad <99866893+WaliMuhammadAhmad@users.noreply.github.com>
2023-10-25 10:02:33 +0500

    Python code for downloading youtube vidoes
    You'd need to install 'pytube' first to runt this code.
    Run this in your terminal
    
    "pip install pytube"
    
    this will install the latest version of pytube in your venv

--- yt_video_downloader.py
+++ yt_video_downloader.py
@@ -1,16 +1,13 @@
 from pytube import YouTube
 
-# Input the YouTube video URL
 video_url = input("Enter the YouTube video URL: ")
 yt = YouTube(video_url)
 
-# List available streams with their resolutions
 available_streams = yt.streams.filter(progressive=True)
 print("Available video quality options:")
 for i, stream in enumerate(available_streams, start=1):
     print(f"{i}. {stream.resolution}")
 
-# Ask the user to select a video quality option
 while True:
     try:
         choice = int(input("Enter the number of the desired video quality (e.g., 1 for 360p, 2 for 480p, ): "))
@@ -22,11 +19,10 @@ while True:
     except ValueError:
         print("Invalid input. Please enter a number.")
 
-# Specify the download location
-download_path = "../videos"
+download_path = "../videos"    # enter the path where you want to save the mp4 video file
 
-# Download the selected video quality
 selected_stream.download(output_path=download_path)
-print("Downloading in Progress...")
+
+print("Downloading Started!")
 
 print(f"Video downloaded to {download_path}")
