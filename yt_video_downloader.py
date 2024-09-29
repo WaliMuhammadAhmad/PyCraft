70fef6b529259cc35474130d08c36e1a6c813eac
WaliMuhammadAhmad <2021se39@student.uet.edu.pk>
2024-09-29 10:16:02 +0500

    yt video downloader updated

--- yt_video_downloader.py
+++ yt_video_downloader.py
@@ -1,28 +1,12 @@
-from pytube import YouTube
+import yt_dlp
 
-video_url = input("Enter the YouTube video URL: ")
-yt = YouTube(video_url)
+def download_video(url):
+    ydl_opts = {
+        'format': 'best',  # best quality format
+        'outtmpl': './%(title)s.%(ext)s'  # output directory and filename format
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
-available_streams = yt.streams.filter(progressive=True)
-print("Available video quality options:")
-for i, stream in enumerate(available_streams, start=1):
-    print(f"{i}. {stream.resolution}")
-
-while True:
-    try:
-        choice = int(input("Enter the number of the desired video quality (e.g., 1 for 360p, 2 for 480p, ): "))
-        if 1 <= choice <= len(available_streams):
-            selected_stream = available_streams[choice - 1]
-            break
-        else:
-            print("Invalid choice. Please enter a valid option.")
-    except ValueError:
-        print("Invalid input. Please enter a number.")
-
-download_path = "../videos"    # enter the path where you want to save the mp4 video file
-
-selected_stream.download(output_path=download_path)
-
-print("Downloading Started!")
-
-print(f"Video downloaded to {download_path}")
+video_url = 'https://www.youtube.com/watch?v=6c2m0RjwRPY'
+download_video(video_url)
